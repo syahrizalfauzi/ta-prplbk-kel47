6a4bd743f3d743e0922f4b68287127ff3a142e1d
M Dzaky Naufal <muhammaddzakynaufaln@gmail.com>
2021-10-06 09:11:06 +0800

    detail styles

--- src/components/ProductDetailModal.jsx
+++ src/components/ProductDetailModal.jsx
@@ -15,14 +15,7 @@ const ProductDetailModal = ({
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       style={{
-        content: {
-          top: "50%",
-          left: "50%",
-          right: "auto",
-          bottom: "auto",
-          marginRight: "-50%",
-          transform: "translate(-50%, -50%)",
-        },
+
       }}
     >
       <div
